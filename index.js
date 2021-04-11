2fab580f2586a85b93ff05515255311714fcbdb7
Scott Strittmatter <scott.a.strittmatter@gmail.com>
2021-04-11 18:59:44 -0500

    Installed fs, and wrote CSS and sample index.html file to set styling.

---
+++ index.js
@@ -0,0 +1,6 @@
+const Inquirer = require('inquirer');
+const fs = require('fs');
+
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
