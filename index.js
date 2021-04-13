4c89e21ddbc62ec23e35a6697c0f4efd5fb3f5cf
Scott Strittmatter <scott.a.strittmatter@gmail.com>
2021-04-13 09:26:01 -0500

    Put all of the html generation in index.js, and have everything populating. Need to tweak the html cards still.

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
-const renderPage = require('./dist/html-generator');
 
 let team = [];
 
@@ -17,7 +16,7 @@ function getManager() {
                 name: 'managerName',
                 message: "What is the team manager's name?",
                 validate: answer => {
-                    if (answer === ''){
+                    if (answer === '') {
                         console.log('Hmm, enter a valid here.');
                         return false;
                     }
@@ -44,7 +43,7 @@ function getManager() {
             const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
             team.push(manager);
             additionalTeamMember();
-        }) 
+        })
 };
 
 function addEngineer() {
@@ -55,7 +54,7 @@ function addEngineer() {
                 name: 'engineerName',
                 message: "What is the engineer's name?",
                 validate: answer => {
-                    if (answer === ''){
+                    if (answer === '') {
                         console.log('Hmm, enter a valid here.');
                         return false;
                     }
@@ -82,7 +81,7 @@ function addEngineer() {
             const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
             team.push(engineer);
             additionalTeamMember();
-        }) 
+        })
 }
 
 function addIntern() {
@@ -93,7 +92,7 @@ function addIntern() {
                 name: 'internName',
                 message: "What is the intern's name?",
                 validate: answer => {
-                    if (answer === ''){
+                    if (answer === '') {
                         console.log('Hmm, enter a valid here.');
                         return false;
                     }
@@ -120,21 +119,21 @@ function addIntern() {
             const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
             team.push(intern);
             additionalTeamMember();
-        }) 
+        })
 }
 
 function additionalTeamMember() {
     Inquirer
-    .prompt([
-        {
-            type: 'list',
-            name: 'memberTitle',
-            message: "Would you like to add another team member?",
-            choices: ['Engineer', 'Intern', 'No Thanks, all done!']
-        }])
+        .prompt([
+            {
+                type: 'list',
+                name: 'memberTitle',
+                message: "Would you like to add another team member?",
+                choices: ['Engineer', 'Intern', 'No Thanks, all done!']
+            }])
         .then(answer => {
             switch (answer.memberTitle) {
-                case "Engineer": 
+                case "Engineer":
                     addEngineer();
                     break;
                 case "Intern":
@@ -143,16 +142,108 @@ function additionalTeamMember() {
                 case "No Thanks, all done!":
                     buildTeam();
                     console.log("Your team has been created! Navigate to ./dist/index.html to see the result.")
-                    console.log(team);
+                    console.log(team[0].name);
+                    console.log(team[0]);
+
                     break;
             }
         });
 };
 
-function buildTeam() {
-    fs.writeFileSync('./dist/index.html', renderPage(team), 'UTF-8')
-}
+let html = '';
 
+
+
+function generateHtml() {
+
+    function getCardHtml() {
+        let cardHtml = '';
+
+        for (let i = 0; i < team.length; i++) {
+            let role = team[i].getRole();
+            let thirdItem = ''
+
+            switch (role) {
+                case 'Manager':
+                    thirdItem = 'Manager'
+                    break;
+                case 'Engineer':
+                    thirdItem = 'Engineer'
+                    break;
+                case 'Intern':
+                    thirdItem = 'Intern'
+                    break;
+            };
+
+            // if (role === 'Manager') {
+            //     thirdItem = 'YEAH IT WORKED'
+            // } else {
+            //     thirdItem = 'Kinda worked?'
+            // };
+
+            let newCard = `<div class="card" style="width: 18rem;">
+           <div class="card-body bg-primary text-white">
+               <h5 class="card-title">${team[i].name}</h5>
+               <h6 class="card-text">${role}</h6>
+           </div>
+           <ul class="list-group list-group-flush">
+               <li class="list-group-item">${team[i].id}</li>
+               <li class="list-group-item">${team[i].email}</li>
+               <li class="list-group-item">${thirdItem}</li>
+           </ul>
+       </div>`;
+            cardHtml = cardHtml.concat(newCard).concat('\n');
+        }
+        return cardHtml;
+    }
+    html = `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+        <!-- Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    
+        <!-- My CSS -->
+        <link rel="stylesheet" href="./style.css">
+        <title>Team Profile</title>
+    
+    </head>
+    
+    <body>
+    
+        <header class="text-center bg-danger d-flex justify-content-center align-items-center">My Team</header>
+    
+        <main>
+    
+            <div class="d-flex flex-wrap justify-content-center align-items-center">
+    
+                ${getCardHtml()}
+                
+            </div>
+        </main>
+    
+        <!-- Boostrap JS -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
+            crossorigin="anonymous"></script>
+    </body>
+    
+    </html>`
+
+
+};
+
+function buildTeam() {
+    generateHtml();
+    fs.writeFileSync('./dist/index.html', html, 'UTF-8')
+
+
+}
 
 // Function call to initialize the app
 getManager();
